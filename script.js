f04492595bf73fabd12f6f787173a02061f6f59b
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2025-08-05 16:54:25 -0400

    Add clearGrid and changeGridButton
    changeGridButton lets user change the number of
    squares per side in the grid.
    
    clearGrid removes all squares from any
    given grid.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const GRID_SIDE_LENGTH = "480px"; 
+const MAX_N = 100;
 
 function createGrid(g, n) {
-    // create an n x n grid of squares in the flexbox div g
+    // create an n x n grid of squares in the flexbox div ("grid") g
     for (let i = 0; i < n; i++){
         const row = document.createElement("div");
         row.classList = "row";
@@ -16,8 +17,26 @@ function createGrid(g, n) {
     }
 }
 
+function clearGrid(g) {
+    // g is a flexbox div
+    while(g.firstChild){
+        g.removeChild(g.firstChild);
+    } 
+}
+
 const grid = document.querySelector("#grid");
 grid.style.height = GRID_SIDE_LENGTH;
 grid.style.width = GRID_SIDE_LENGTH;
 
-createGrid(grid, 16);
+let n = 16;
+createGrid(grid, n);
+
+const changeGridButton = document.querySelector("#changeGridButton");
+changeGridButton.addEventListener("click", () => {
+    let input = +prompt("What's the number of squares per side for the new grid?");
+    n = Math.min(input, MAX_N);
+    clearGrid(grid);
+    createGrid(grid, n);
+})
+
+
